9954b672cbf567db85a5bac58eb07f755e433b44
Richard Gabaree <richard.gabaree@gmail.com>
2021-03-16 17:47:46 -0500

    previous commits done in component file

---
+++ src/resumeData.js
@@ -0,0 +1,17 @@
+let resumeData = {
+  
+"imagebaseurl":"",
+"name": "Richard Gabaree",
+"role": "Full Stack Developer/Marketer",
+"linkedinId":"",
+"roleDescription": "",
+"socialLinks": [
+  {
+    "name":"LinkedIn",
+    "url":"https://",
+    "className":"fa fa-linkedin"
+  },
+
+  
+]
+}
